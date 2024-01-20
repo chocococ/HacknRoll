81137d673360f253e0b825e75d8d9ffeb21bc4ee
le xuan <e0969645@u.nus.edu>
2024-01-20 17:03:46 +0800

    Initial commit

---
+++ app/src/main/java/com/example/quizmodoro/Question.kt
@@ -0,0 +1,43 @@
+package com.example.quizmodoro
+
+class Question {
+    // This is a simplified representation of your network call to fetch questions.
+// You would use Retrofit or another HTTP library to actually implement this.
+//    fun fetchQuestionsFromGoogleBard() {
+//        // Make an API call to Google Bard to get questions
+//        val response = makeApiCallToGoogleBard()
+//
+//        // Parse the response and create Question objects
+//        val questionsList = parseResponseToQuestions(response)
+//
+//        // Use the questionsList to update your UI
+//        updateQuizUI(questionsList)
+//    }
+//
+//    fun parseResponseToQuestions(response: ApiResponse): ArrayList<Question> {
+//        val questionsList = ArrayList<Question>()
+//
+//        // Assuming the response from Google Bard is in the form of a list of question data
+//        for (questionData in response.questions) {
+//            val question = Question(
+//                id = questionData.id,
+//                question = questionData.questionText,
+//                image = getDrawableIdByName(questionData.imageName), // You'll need to map image names to drawable resources
+//                optionOne = questionData.options[0],
+//                optionTwo = questionData.options[1],
+//                optionThree = questionData.options[2],
+//                optionFour = questionData.options[3],
+//                correctAnswer = questionData.correctAnswerIndex
+//            )
+//            questionsList.add(question)
+//        }
+//
+//        return questionsList
+//    }
+//
+    // This function would update your quiz UI with the new questions.
+//    fun updateQuizUI(questions: ArrayList<Question>) {
+//        // Logic to display questions in the UI
+//    }
+
+}
